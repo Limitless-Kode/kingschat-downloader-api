7f9b36fd264bc94c658a93671ca20c8296a945a5
Peter Claver Amobila <57482239+Limitless-Kode@users.noreply.github.com>
2021-10-21 12:03:21 +0000

    puppeteer args

--- helpers/Scapper.js
+++ helpers/Scapper.js
@@ -1,7 +1,10 @@
 const puppeteer = require("puppeteer");
 
 const getImage = async (url) => {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({'args' : [
+    '--no-sandbox',
+    '--disable-setuid-sandbox'
+  ]});
     const page = await browser.newPage();
     await page.goto(url);
 
