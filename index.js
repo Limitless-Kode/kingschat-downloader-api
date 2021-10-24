eb9dda212ae2d050fdc0a79d99b7178d6a97cc86
Peter Claver Amobila <57482239+Limitless-Kode@users.noreply.github.com>
2021-10-24 15:17:05 +0000

    cors fix

--- index.js
+++ index.js
@@ -3,23 +3,11 @@ const app = require('express')();
 const cors = require('cors');
 const bodyParser = require('body-parser')
 
-const jsonParser = bodyParser.json()
+const jsonParser = bodyParser.json();
 
- app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-    );
-  if (req.method == "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
-    return res.status(200).json({});
-  }
-
-  next();
-});
-
-app.use(cors());
+app.use(cors({
+    origin: '*'
+}));
 
 app.get('/', (req, res) => {
     res.send("Hello World");
